1a44c7265f9d276727ef8cd92deb0bd15f92994c
Dominik Lenoch <dlenoch@redhat.com>
2019-09-17 13:31:58 +0200

    Fix shebang lines from sh to bash

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 REGISTRY=quay.io/interconnectedcloud
 IMAGE=qdr-operator
 TAG=1.0.0-beta6
 
 if [[ -z ${CI} ]]; then
-	./hack/go-test.sh
-	operator-sdk build ${REGISTRY}/${IMAGE}:${TAG}
+    ./hack/go-test.sh
+    operator-sdk build ${REGISTRY}/${IMAGE}:${TAG}
 else
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -o build/_output/bin/qdr-operator github.com/interconnectedcloud/qdr-operator/cmd/manager
 fi
